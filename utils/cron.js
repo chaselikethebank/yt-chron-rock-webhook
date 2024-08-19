14f6cff1857e8c946e41a531e899ef85c3902b27
cd.t <chasedemaster.tech@gmail.com>
2024-08-19 15:17:17 -0500

    cleaned cron

--- utils/cron.js
+++
@@ -1,8 +0,0 @@
-const cron = require('node-cron');
-const fetchYouTubeData = require('../fetch');  
-
-// Schedule the cron job to run weekly
-cron.schedule('*/10 * * * * *', () => {
-  console.log('Running YouTube data fetch job...');
-  fetchYouTubeData();
-});
