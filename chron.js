4c1096c7cd1adaec721b7b1150fbe92848e5865c
cd.t <chasedemaster.tech@gmail.com>
2024-09-16 13:26:18 -0500

    prepping for vm

--- chron.js
+++ chron.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const fetchYouTubeData = require('./fetch-refactor');  
 
 // Schedule the cron job to run weekly
-cron.schedule('*/10 * * * * *', () => {
+cron.schedule('30 13 * * 1', () => {
   console.log('Running YouTube data fetch job...');
   fetchYouTubeData();
 
@@ -11,3 +11,9 @@ cron.schedule('*/10 * * * * *', () => {
 
 //  to run the job every Tuesday and Friday at 1:00 pm cst 
 // 0 13 * * 2,5
+
+// monday at 1:30 
+// 30 13 * * 1
+
+//every 10 seconds 
+// */10 * * * * *
