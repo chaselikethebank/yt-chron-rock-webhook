b98c00a808d09070b0d551dc8451c0399f191372
cd.t <chasedemaster.tech@gmail.com>
2024-09-16 16:00:37 -0500

    working chron times

--- chron.js
+++ chron.js
@@ -1,19 +1,35 @@
 const cron = require('node-cron');
 const fetchYouTubeData = require('./fetch-refactor');  
 
-// Schedule the cron job to run weekly
-cron.schedule('0 13 * * 2,5', () => {
-  console.log('Running YouTube data fetch job...');
+// Schedule the cron job to run at 2:30 PM from Monday to Friday
+cron.schedule('30 14 * * 1,2,3,4,5', () => {
+  console.log('Running YouTube data fetch job at 2:30 PM...');
   fetchYouTubeData();
-
 });
 
+// Schedule the cron job to run at 9:30 AM from Monday to Friday
+cron.schedule('30 9 * * 1,2,3,4,5', () => {
+  console.log('Running YouTube data fetch job at 9:30 AM...');
+  fetchYouTubeData();
+});
 
-//  to run the job every Tuesday and Friday at 1:00 pm cst 
-// 0 13 * * 2,5
+/*
+Cron expressions are used to schedule tasks based on time. Here’s a breakdown of each field in a cron expression:
 
-// monday at 1:30 
-// 30 13 * * 1
+Field          Description                      Meaning
+*              Minute                            The job will run at every minute.
+*              Hour                              The job will run at every hour.
+*              Day of Month                      The job will run every day of the month.
+*              Month                             The job will run every month of the year.
+2,5            Day of Week                       The job will run on specified days of the week.
 
+Notes:
+- `*` means "every" or "any" value for that field.
+- Numbers specify specific values.
+- Lists (e.g., `2,5`) specify multiple values.
+- Ranges and other special characters can be used for more complex schedules.
+*/
+
+ 
 //every 10 seconds 
 // */10 * * * * *
